33724019cfc2fea8faac0d50734e4f8429e87da7
ChauQuocTuan06 <chauquoctuan160306@gmail.com>
2026-02-24 21:19:26 +0700

    Create blinkled.py

---
+++ blinkled.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+while True:
+  GPIO.output(17,True)
+  sleep(4)
+  GPIO.output(17,False)
+  sleep(2)
