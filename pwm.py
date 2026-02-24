a10d417446977cf9a19fab9d3a32ddf780624996
ChauQuocTuan06 <chauquoctuan160306@gmail.com>
2026-02-24 21:30:31 +0700

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -1,1 +1,18 @@
-
+import RPi.GPIO as GPIO
+from time import sleep
+ledPin = 4
+try:
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(ledPin,GPIO.OUT)
+  pwm-GPIO.PWM(ledPin,100)
+  pwm.start(0)
+  while True:
+    for duty in range (100):
+      print (duty)
+      pwm.changeDutyCycle(duty)
+      sleep(0.2)
+      if(duty>100) : duty = 0
+except KeyboardInterrupt:
+  print("Complete")
+finally:
+  GPIO.cleanup()
