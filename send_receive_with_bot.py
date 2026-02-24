d9010fb1be98bf342a1df7c37aead440ff13cad5
ChauQuocTuan06 <chauquoctuan160306@gmail.com>
2026-02-24 22:54:05 +0700

    Update send_receive_with_bot.py

--- send_receive_with_bot.py
+++ send_receive_with_bot.py
@@ -1,15 +1,20 @@
-
 import RPi.GPIO as GPIO
-import requests
 from telegram.ext import ApplicationBuilder, CommandHandler
+import requests
 from time import sleep
+import threading
+
 TOKEN = "8120771917:AAGBwURu68ZVwN8oczrReAizuPDE-VdQS14"
 CHAT_ID = "8411566618"
+
 LED = 4
 VIB = 27
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(VIB, GPIO.IN)
+
+
 async def led_on(update, context):
     GPIO.output(LED, 1)
     await update.message.reply_text("LED ON")
@@ -17,11 +22,6 @@ async def led_on(update, context):
 async def led_off(update, context):
     GPIO.output(LED, 0)
     await update.message.reply_text("LED OFF")
-  
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("on", led_on))
-app.add_handler(CommandHandler("off", led_off))
-app.run_polling()
 
 
 def sendTelegram(text):
@@ -30,9 +30,18 @@ def sendTelegram(text):
         "chat_id": CHAT_ID,
         "text": text
     }
+    requests.post(url, data=data)
 
-    requests.post(url,data=data)
-while True:
-    if GPIO.input(VIB)==1:
-        sendTelegram("Open the door")
-        sleep(5)
+
+def check_vibration():
+    while True:
+        if GPIO.input(VIB) == 1:
+            sendTelegram("Open the door")
+            sleep(5)
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("on", led_on))
+app.add_handler(CommandHandler("off", led_off))
+threading.Thread(target=check_vibration).start()
+app.run_polling()
